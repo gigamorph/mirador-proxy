758996f9a8e772694235d186cd7a01738106cfdc
Seong-June Kim <seong-june.kim@yale.edu>
2017-11-27 15:46:56 -0500

    Fix export

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,10 @@
-import MiradorProxyManager from './mirador-proxy-manager';
+import getMiradorProxyManager from './mirador-proxy-manager';
 import MiradorProxy from './mirador-proxy';
 import WindowProxy from './window-proxy';
 import WorkspaceProxy from './workspace-proxy';
 
 window.MiradorProxy = {
-  MiradorProxyManager: MiradorProxyManager,
+  getMiradorProxyManager: getMiradorProxyManager,
   MiradorProxy: MiradorProxy,
   WindowProxy: WindowProxy,
   WorkspaceProxy: WorkspaceProxy
